668f5c9b89f5de9a383f727ca7afaa6e385a1503
charanbot123 <cs19047@velammalitech.edu.in>
2024-07-16 12:48:46 +0530

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testAdd() {
+        assertEquals(5, App.add(2, 3));
+    }
+}
